9f2d99911223c0c047e50802c011079fcf7866c4
SyedHassan1212 <nh31097@gmail.com>
2025-08-16 15:48:45 +0500

    added memo for rerender

--- week-06-deep-react/week6-deep/src/App.jsx
+++ week-06-deep-react/week6-deep/src/App.jsx
@@ -2,36 +2,45 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
 
 function App() {
+  const [title, setTitle] = useState("my name is hassan")
+function setTitle1(){
+  setTitle("my name is" + Math.random())
+}
 
-return (
+  return (
     <div> 
-      <Headerwithbutton></Headerwithbutton>
+    <button onClick={setTitle1} > update</button>
+    <Header title={title} />
       <Header title='hassan'></Header>
       <Header title = "hassan"></Header>
 
     </div>
   )
 }
-//reRender with whole headers being reloaded again
- function Headerwithbutton(){
-const [title, setTitle] = useState("my name is hassan")
-function setTitle1(){
-  setTitle("my name is" + Math.random())
-}
-return (
-    <div> 
-    <button onClick={setTitle1} > update</button>
-      <Header title={title}  ></Header>
-      </div>
-  )
-}
 
+//first method make differnet fn to render 
+
+// function Rerender(){
+//   const [title, setTitle] = useState("my name is hassan")
+// function setTitle1(){
+//   setTitle("my name is" + Math.random())
+// }
+// return <div>
+//   <button onClick={setTitle1} > update</button>
+//     <Header title={title} />
+// </div>
+
+// }
+
+//use keyword memo
+const Header = React.memo(
 function Header({title}){
   return <div>
     {title}
   </div>
-}
+})
 
 export default App
