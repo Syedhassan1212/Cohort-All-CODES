1692620036c9c1de43ddfe0fc28592cec73d7dcd
SyedHassan1212 <nh31097@gmail.com>
2025-08-16 14:56:12 +0500

    added reRender

--- week-06-deep-react/week6-deep/src/App.jsx
+++ week-06-deep-react/week6-deep/src/App.jsx
@@ -4,21 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [title, setTitle] = useState("my name is hassan")
-function setTitle1(){
-  setTitle("my name is" + Math.random())
-}
 
-  return (
+return (
     <div> 
-    <button onClick={setTitle1} > update</button>
-    <Header title={title} />
       <Header title='hassan'></Header>
       <Header title = "hassan"></Header>
 
     </div>
   )
 }
+//reRender with whole headers being reloaded again
+ function Headerwithbutton(){
+const [title, setTitle] = useState("my name is hassan")
+function setTitle1(){
+  setTitle("my name is" + Math.random())
+}
+return (
+    <div> 
+    <button onClick={setTitle1} > update</button>
+      <Header title={title}  ></Header>
+      </div>
+  )
+}
+
+
+
 
 function Header({title}){
   return <div>
