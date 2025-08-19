a20db19f19ce9faa79f60702a1cd003bfac0eeb3
SyedHassan1212 <nh31097@gmail.com>
2025-08-19 16:25:14 +0500

    started cardWrappers

--- week-06-deep-react/week6-deep/src/App.jsx
+++ week-06-deep-react/week6-deep/src/App.jsx
@@ -1,46 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import React from 'react'
+// import { useState } from 'react'
+// import './App.css'
+// import React from 'react'
 
-function App() {
-  const [title, setTitle] = useState("my name is hassan")
-function setTitle1(){
-  setTitle("my name is" + Math.random())
-}
-
-  return (
-    <div> 
-    <button onClick={setTitle1} > update</button>
-    <Header title={title} />
-      <Header title='hassan'></Header>
-      <Header title = "hassan"></Header>
-
-    </div>
-  )
-}
-
-//first method make differnet fn to render 
-
-// function Rerender(){
+//function App() {
 //   const [title, setTitle] = useState("my name is hassan")
 // function setTitle1(){
 //   setTitle("my name is" + Math.random())
 // }
-// return <div>
-//   <button onClick={setTitle1} > update</button>
-//     <Header title={title} />
-// </div>
 
+//   return (
+//     <div> 
+//     <button onClick={setTitle1} > update</button>
+//     <Header title={title} />
+//       <Header title='hassan'></Header>
+//       <Header title = "hassan"></Header>
+
+//     </div>
+//   )
 // }
 
-//use keyword memo
-const Header = React.memo(
-function Header({title}){
-  return <div>
-    {title}
-  </div>
-})
+// //first method make differnet fn to render 
 
+// // function Rerender(){
+// //   const [title, setTitle] = useState("my name is hassan")
+// // function setTitle1(){
+// //   setTitle("my name is" + Math.random())
+// // }
+// // return <div>
+// //   <button onClick={setTitle1} > update</button>
+// //     <Header title={title} />
+// // </div>
+
+// // }
+
+// //use keyword memo
+// const Header = React.memo(
+// function Header({title}){
+//   return <div>
+//     {title}
+//   </div>
+// })
+
+
+
+
+
+//building wrapper
+function App(){
+
+
+
+}
 export default App
